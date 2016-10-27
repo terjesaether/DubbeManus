deba027eaff12d4ebdaa35911eac762e2da34c3a
terje <terje@MBP-WIN>
2016-10-27 19:00:02 +0200

    Add project files.

---
+++ DubbeManus/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DubbeManus.MvcApplication" Language="C#" %>
---
+++ DubbeManus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DubbeManus.Startup))]
+namespace DubbeManus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
